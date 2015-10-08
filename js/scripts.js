32f37c4283990e1f0992d830e8cf1e33ba816936
Julie Field <jcave@greenlinecreative.com>
2015-10-08 18:03:08 -0400

    created new pricing and compare pricing pages, updated medical images and copy

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,58 @@ $( "#phonetop_link" ).click(function() {
   $( "#phoneTop" ).slideToggle( "fast", function() {
   });
 });
+
+// show snippet source code
+var $button = $("<div id='source-button' title='See the HTML source code.' class='btn btn-default btn-xs'><i class='fa fa-2x fa-code'></i></div>").click(function(){
+  var html = $(this).parent().html();
+  html = cleanSource(html);
+  $("#source-modal textarea").text(html);
+  $("#source-modal").modal();
+}).tooltip({placement:'left'});
+
+$(".show-source").hover(function(){
+  $(this).append($button);
+  $button.show();
+}, function(){
+  $button.hide();
+});
+
+$("#btnCopy").click(function(){
+
+  selectText('sourceCode');
+  
+});
+
+function selectText(containerid) {
+  if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select();
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+  }
+}
+
+function cleanSource(html) {
+  var lines = html.split(/\n/);
+  
+  lines.shift();
+  lines.splice(-1, 1);
+  
+  var indentSize = lines[0].length - lines[0].trim().length,
+    re = new RegExp(" {" + indentSize + "}");
+  
+  lines = lines.map(function(line){
+    if (line.match(re)) {
+      line = line.substring(indentSize);
+    }
+    return line;
+  });
+  
+  lines = lines.join("\n");  
+  return lines;
+}
+
+
